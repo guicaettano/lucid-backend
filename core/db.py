ae9077394f9f8d99faac0ce59f45a37489c9a24f
Guilherme Caetano Dos Santos Da Mata <guicaetanofalcon@gmail.com>
2025-04-18 01:21:45 -0300

    commit

--- core/db.py
+++ core/db.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy import Column, String, Text, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base  # Atualizado aqui
 from datetime import datetime
@@ -16,6 +17,11 @@ class Documento(Base):
     conteudo = Column(Text)  # ⬅️ Adicione essa linha
     timestamp = Column(DateTime, default=datetime.now)
 
+# Recria o banco de dados
+if os.path.exists("lucid.db"):
+    os.remove("lucid.db")
+    print("🗑️ Banco de dados excluído.")
 engine = create_engine("sqlite:///lucid.db")
 Base.metadata.create_all(engine)
+print("✅ Novo banco de dados criado.")
 Session = sessionmaker(bind=engine)
